41d9b9ad2bd8c14a8861e73098f7baf65e03956d
Alejandro Viñas <114479197+AlejandroVinas@users.noreply.github.com>
2023-05-09 16:05:04 +0200

    Add files via upload

--- Guerrero-Vampiro/Guerrero_Vampiro/src/main/Weapons.java
+++ Guerrero-Vampiro/Guerrero_Vampiro/src/main/Weapons.java
@@ -1,36 +1,26 @@
 package main;
-    public class Weapons {
-        private int damage;
-        private double accuracy;
 
-        public Weapons(int damage, double accuracy) {
-            this.damage = damage;
-            this.accuracy = accuracy;
-        }
+public abstract class Weapons {
+    private int damage;
+    private double accuracy;
 
-        public int getDamage() {
+    public Weapons(int damage, double accuracy) {
+        this.damage = damage;
+        this.accuracy = accuracy;
+    }
+
+    public int getDamage() {
             return damage;
         }
-
-        public void setDamage(int damage) {
+    public void setDamage(int damage) {
             this.damage = damage;
         }
 
-        public double getAccuracy() {
+    public double getAccuracy() {
             return accuracy;
         }
 
-        public void setAccuracy(double accuracy) {
+    public void setAccuracy(double accuracy) {
             this.accuracy = accuracy;
         }
-
-
-//Weapons sword = new Weapons(7, 50);
-//Weapons handaxe = new Weapons(15, 25);
-//Weapons flail = new Weapons(30, 12);
-//Weapons bite = new Weapons(5, 90);
-//Weapons scratch = new Weapons(10, 60);
-//Weapons soulsiphon = new Weapons(20, 40);
-//Weapons shield = new Weapons(0, 80);
-
 }
