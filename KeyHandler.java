d551d6ce96f6eef555ba3297b6d1bb20161b8237
David Montenegro <68624139+Dmon4516@users.noreply.github.com>
2023-05-09 13:01:34 +0200

    feature(a, w, s, d): input added

--- KeyHandler.java
+++ KeyHandler.java
@@ -11,32 +11,32 @@ public class KeyHandler implements KeyListener {
  @Override
  public void keyPressed(KeyEvent e) {
      int code = e.getKeyCode();
-     if(code == KeyEvent.VK_UP) {
+     if(code == KeyEvent.VK_UP || code == KeyEvent.VK_W) {
          upPressed = true;
      }
-     if(code == KeyEvent.VK_DOWN) {
+     if(code == KeyEvent.VK_DOWN || code == KeyEvent.VK_S) {
             downPressed = true;
      }
-     if(code == KeyEvent.VK_LEFT) {
+     if(code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A){
               leftPressed = true;
      }
-     if(code == KeyEvent.VK_RIGHT) {
+     if(code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D) {
                   rightPressed = true;
      }
  }
  @Override
  public void keyReleased(KeyEvent e) {
   int code = e.getKeyCode();
-  if(code == KeyEvent.VK_UP) {
+  if(code == KeyEvent.VK_UP || code == KeyEvent.VK_W) {
    upPressed = false;
   }
-  if(code == KeyEvent.VK_DOWN) {
+  if(code == KeyEvent.VK_DOWN || code == KeyEvent.VK_S) {
    downPressed = false;
     }
-  if(code == KeyEvent.VK_LEFT) {
+  if(code == KeyEvent.VK_LEFT || code == KeyEvent.VK_A){
    leftPressed = false;
   }
-  if(code == KeyEvent.VK_RIGHT) {
+  if(code == KeyEvent.VK_RIGHT || code == KeyEvent.VK_D) {
    rightPressed = false;
   }
  }
