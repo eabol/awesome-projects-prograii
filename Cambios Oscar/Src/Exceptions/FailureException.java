f99883e1b29964e3c00337bdcfd0894e4b1a12a0
Oscar Santamaria <114479802+Oscarsantasanchez@users.noreply.github.com>
2023-06-05 20:53:06 +0200

    Create FailureException.java

---
+++ Cambios Oscar/Src/Exceptions/FailureException.java
@@ -0,0 +1,22 @@
+package Exceptions;
+
+import Interface.Incidence;
+
+import java.time.LocalDate;
+
+public class FailuireException extends Exception implements Incidence {
+    private int idMachine;
+    private LocalDate date;
+
+    public FailuireException(int idMachine) {
+        super("Se ha producido una Averia en la máquina "+ idMachine +"");
+        this.idMachine = idMachine;
+        setDate();
+    }
+
+
+    @Override
+    public void setDate() {
+        this.date = LocalDate.now();
+    }
+}
