e0774355b9fc05fcc46c78a32f2fabc14826caab
David Montenegro <68624139+Dmon4516@users.noreply.github.com>
2023-06-05 11:22:45 +0200

    Revert "Revert "Merge pull request #5 from VeronikaEspa/jorgeQuinonez""
    This reverts commit 81b6f1d26e199274fa01742ed08327fc30ac2a25.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,11 +1,12 @@
 package src.entity;
+
 import java.awt.image.BufferedImage;
 
 public class Entity {
-public int x, y, speed;
-public BufferedImage upLeft, upRight, left1, right1, left2, left3, right2, right3, up1, up2;
-public String direction;
-public int spriteCounter = 0;
-public int spriteNum = 1;
+    public int x, y, speed;
+    public BufferedImage upLeft, upRight, left1, right1, left2, left3, right2, right3, up1, up2;
+    public String direction;
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 
 }
