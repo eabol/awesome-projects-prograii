d9272512d4ef03187b9187c37dc248d9d3831b77
Santiago Gomez de la Torre <santiago.gomez1@alumnos.uneatlantico.es>
2023-06-08 12:03:53 +0200

    Feature/ Added project models

--- Mrs_Gts/src/models/MachineModel.java
+++ Mrs_Gts/src/models/MachineModel.java
@@ -1,4 +1,45 @@
 package models;
 
+import java.util.List;
+
 public class MachineModel {
+    private String name;
+    private List<CoinModel> coins;
+    private List<BillModel> bills;
+    private List<ProductModel> products;
+
+    public MachineModel(String name, List<CoinModel> coins, List<BillModel> bills, List<ProductModel> products) {
+        this.name = name;
+        this.coins = coins;
+        this.bills = bills;
+        this.products = products;
+    }
+
+    public void addCoins(CoinModel coin) {
+        coins.add(coin);
+    }
+
+    public void addBills(BillModel bill) {
+        bills.add(bill);
+    }
+
+    public void addProducts(ProductModel product) {
+        products.add(product);
+    }
+
+    public String showName() {
+        return name;
+    }
+
+    public List<CoinModel> listOfCoins() {
+        return coins;
+    }
+
+    public List<BillModel> listOfBills() {
+        return bills;
+    }
+
+    public List<ProductModel> listOfProducts() {
+        return products;
+    }
 }
