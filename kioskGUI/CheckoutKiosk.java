fca952a4ef9b5a627ffe47a90c89ef9e29941b0a
0xJVR <114479588+0xJVR@users.noreply.github.com>
2023-06-09 17:58:41 +0200

    Update GUI to V2

--- kioskGUI/CheckoutKiosk.java
+++ kioskGUI/CheckoutKiosk.java
@@ -8,10 +8,15 @@ public class CheckoutKiosk extends JFrame {
     private ProductList productList;
     private DiscountList discountList;
     private JList<String> productJList;
-    private JTextArea totalPriceTextArea;
+    private JList<String> cartJList;
+    private JLabel totalPriceLabel;
     private JTextField discountCodeField;
     private ArrayList<Product> selectedProducts;
     private JLabel messageLabel;
+    private DefaultListModel<String> cartModel;
+    private JLabel cartTotalLabel;
+    private JLabel discountValueLabel;
+    private JLabel newTotalLabel;
 
     public CheckoutKiosk() {
         productList = new ProductList();
@@ -21,20 +26,19 @@ public class CheckoutKiosk extends JFrame {
     }
 
     private void setupGUI() {
-        setSize(500, 500);
+        setSize(720, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
 
-        messageLabel = new JLabel("");
-        add(messageLabel, BorderLayout.NORTH);
-
+        // Create a main panel with CardLayout.
         JPanel mainPanel = new JPanel(new CardLayout());
         add(mainPanel, BorderLayout.CENTER);
 
         JPanel welcomePanel = new JPanel();
         JLabel welcomeLabel = new JLabel("Welcome to the self checkout kiosk!");
-        JButton nextButton = new JButton("Next");
+        JButton startButton = new JButton("Start");
 
-        nextButton.addActionListener(new ActionListener() {
+        startButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 CardLayout cl = (CardLayout) mainPanel.getLayout();
                 cl.next(mainPanel);
@@ -42,33 +46,52 @@ public class CheckoutKiosk extends JFrame {
         });
 
         welcomePanel.add(welcomeLabel);
-        welcomePanel.add(nextButton);
+        welcomePanel.add(startButton);
 
-        JPanel productListPanel = new JPanel();
+        JPanel shopPanel = new JPanel(new BorderLayout());
         productJList = new JList<>();
-        JButton nextButton2 = new JButton("Next");
+        productJList.setPrototypeCellValue("Item Name --------------------");
+        cartModel = new DefaultListModel<>();
+        cartJList = new JList<>(cartModel);
+        cartJList.setPrototypeCellValue("Item Name --------------------");
+        JButton addToCartButton = new JButton("Add to cart");
+        
+        cartTotalLabel = new JLabel("Cart total: ");
+        JButton checkoutButton = new JButton("Checkout");
 
-        nextButton2.addActionListener(new ActionListener() {
+        addToCartButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            for (String productInfo : productJList.getSelectedValuesList()) {
-                String productName = productInfo.split(":")[0].trim();
-                Product product = productList.getItemByName(productName);
-                if (product != null) {
-                    selectedProducts.add(product);
+                for (String productInfo : productJList.getSelectedValuesList()) {
+                    String productName = productInfo.split(":")[0].trim();
+                    Product product = productList.getItemByName(productName);
+                    if (product != null) {
+                        selectedProducts.add(product);
+                        cartModel.addElement(product.getName() + ": " + product.getPrice());
+                        updateCartTotal();
+                    }
                 }
             }
-            CardLayout cl = (CardLayout) mainPanel.getLayout();
-            cl.next(mainPanel);
-    }
         });
 
-        productListPanel.add(new JScrollPane(productJList));
-        productListPanel.add(nextButton2);
+        checkoutButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) mainPanel.getLayout();
+                cl.next(mainPanel);
+            }
+        });
 
-        JPanel discountPanel = new JPanel();
+        shopPanel.add(new JScrollPane(productJList), BorderLayout.WEST);
+        shopPanel.add(addToCartButton, BorderLayout.CENTER);
+        shopPanel.add(new JScrollPane(cartJList), BorderLayout.EAST);
+        shopPanel.add(cartTotalLabel, BorderLayout.SOUTH);
+        shopPanel.add(checkoutButton, BorderLayout.SOUTH);
+
+        JPanel checkoutPanel = new JPanel();
         discountCodeField = new JTextField(10);
         JButton applyDiscountButton = new JButton("Apply Discount");
-        JButton nextButton3 = new JButton("Next");
+        discountValueLabel = new JLabel("Discount value: ");
+        newTotalLabel = new JLabel("New total: ");
+        JButton confirmButton = new JButton("Confirm");
 
         applyDiscountButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -78,53 +101,55 @@ public class CheckoutKiosk extends JFrame {
                     for (Product product : selectedProducts) {
                         product.applyDiscount(discount);
                     }
-                    messageLabel.setText("Discount applied successfully!");
-                } else {
-                    messageLabel.setText("Error: Invalid discount code");
+                    discountValueLabel.setText("Discount value: " + discount.getDiscountValue());
+                    updateCartTotal();
+                    applyDiscountButton.setEnabled(false);
                 }
             }
         });
 
-        nextButton3.addActionListener(new ActionListener() {
+        confirmButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 CardLayout cl = (CardLayout) mainPanel.getLayout();
                 cl.next(mainPanel);
             }
         });
 
-        discountPanel.add(new JLabel("Enter discount code: "));
-        discountPanel.add(discountCodeField);
-        discountPanel.add(applyDiscountButton);
-        discountPanel.add(nextButton3);
+        checkoutPanel.add(new JLabel("Enter discount code: "));
+        checkoutPanel.add(discountCodeField);
+        checkoutPanel.add(applyDiscountButton);
+        checkoutPanel.add(discountValueLabel);
+        checkoutPanel.add(newTotalLabel);
+        checkoutPanel.add(confirmButton);
 
-        JPanel totalPricePanel = new JPanel();
-        totalPriceTextArea = new JTextArea(5, 20);
-        JButton finishButton = new JButton("Finish");
+        JPanel thankYouPanel = new JPanel();
+        JLabel thankYouLabel = new JLabel("Thank you for shopping!");
+        JButton exitButton = new JButton("Exit");
 
-        finishButton.addActionListener(new ActionListener() {
+        exitButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if (!selectedProducts.isEmpty()) {
-                    double total = 0;
-                    for (Product product : selectedProducts) {
-                        total += product.getPrice();
-                    }
-                    totalPriceTextArea.setText("Total price: " + total);
-                    messageLabel.setText("");
-                } else {
-                    messageLabel.setText("Error: No products selected");
-                }
+                System.exit(0);
             }
         });
 
-        totalPricePanel.add(new JScrollPane(totalPriceTextArea));
-        totalPricePanel.add(finishButton);
+        thankYouPanel.add(thankYouLabel);
+        thankYouPanel.add(exitButton);
 
         mainPanel.add(welcomePanel, "WELCOME_PANEL");
-        mainPanel.add(productListPanel, "PRODUCT_LIST_PANEL");
-        mainPanel.add(discountPanel, "DISCOUNT_PANEL");
-        mainPanel.add(totalPricePanel, "TOTAL_PRICE_PANEL");
+        mainPanel.add(shopPanel, "SHOP_PANEL");
+        mainPanel.add(checkoutPanel, "CHECKOUT_PANEL");
+        mainPanel.add(thankYouPanel, "THANK_YOU_PANEL");
 
         setVisible(true);
+    }
+
+    private void updateCartTotal() {
+        double total = 0;
+        for (Product product : selectedProducts) {
+            total += product.getPrice();
+        }
+        cartTotalLabel.setText("Cart total: " + total);
+        newTotalLabel.setText("New total: " + total);
     }
 
     public void addProduct(Product product) {
