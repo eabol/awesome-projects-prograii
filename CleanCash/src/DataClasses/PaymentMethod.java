787693bd9387534fed19d285cd6dd50c9d3522b4
0xJVR <114479588+0xJVR@users.noreply.github.com>
2023-06-09 20:44:05 +0200

    Intentado hacer git otra vez, otra vez, otra vez

--- CleanCash/src/DataClasses/PaymentMethod.java
+++
@@ -1,5 +0,0 @@
-package DataClasses;
-
-public abstract class PaymentMethod {
-
-}
--- CleanCash/src/SystemManagement/DataManager.java
+++
@@ -1,9 +0,0 @@
-package SystemManagement;
-
-import DataClasses.Entity;
-
-public interface DataManager {
-    void add(Entity newEntity);
-    void remove(Entity removedEntity);
-    void identify(Entity entity);
-}
