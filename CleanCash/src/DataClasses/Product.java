1f8d220d535dd005590a9781a3b70c742017d18e
joserasilla-uneat <114484500+joserasilla-uneat@users.noreply.github.com>
2023-06-09 14:09:05 +0200

    (FEAT): Progress

--- CleanCash/src/DataClasses/Product.java
+++ CleanCash/src/DataClasses/Product.java
@@ -1,9 +1,15 @@
 package DataClasses;
 public class Product extends Entity{
+    private float price;
 
-    float price;
+    public float getPrice() {
+        return price;
+    }
+
     public Product(String entityName, String entityId, float price) {
         super(entityName, entityId);
         this.price = price;
     }
+
+
 }
