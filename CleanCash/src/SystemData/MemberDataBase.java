79e0e83b51dda2547689f12b8ad9de1054dc9cce
Dievexx <diego30sscc@gmail.com>
2023-06-09 12:10:28 +0200

    small changes

---
+++ CleanCash/src/SystemData/MemberDataBase.java
@@ -0,0 +1,15 @@
+package SystemData;
+
+import DataClasses.Client;
+import DataClasses.Member;
+
+public class MemberDataBase {
+
+    Member[] members = new Member[5];
+    public MemberDataBase() {
+
+
+
+    }
+
+}
