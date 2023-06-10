ac29b026163a803b2162e536f1c24ff91a88e478
Diegoza04 <barrazadiego05@gmail.com>
2023-06-10 21:38:37 +0200

    Updates pt.2

--- Aspiradora P1/Board.java
+++ Aspiradora P1/Board.java
@@ -1,34 +1,57 @@
 import java.util.ArrayList;
 
-;
+
 
     public class Board {
         private Tile[][] matrix;
+        private int rows;
+        private int columns;
 
-        public Board(int rows, int columns, /* Argumentos adicionales para Tile */) {
-            matrix = new Tile[rows][columns];
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[i].length; j++) {
-                    matrix[i][j] = new Tile(/* Argumentos para Tile */);
+        public Board(int rows, int columns) {
+            this.rows = rows;
+            this.columns = columns;
+            this.matrix = new Tile[rows][columns];
+            initializeTiles();
+
+        }
+
+        private void initializeTiles() {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    matrix[i][j] = new Tile(i, j);
                 }
             }
         }
-
-        public ArrayList<Tile> getArrayList() {
-            ArrayList<Tile> tileList = new ArrayList<>();
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[i].length; j++) {
-                    tileList.add(matrix[i][j]);
+        public  Tile getTile(int rows, int columns) {
+            return matrix[rows][columns];
+        }
+        public void setTile(int rows, int columns, Tile tile) {
+            matrix[rows][columns] = tile;
+        }
+        public int getRows() {
+            return rows;
+        }
+        public int getColumns() {
+            return columns;
+        }
+        public ArrayList<Tile>getAllTiles(){
+            ArrayList<Tile> tiles = new ArrayList<>();
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    tiles.add(matrix[i][j]);
                 }
             }
-            return tileList;
+            return tiles;
         }
-
-        public void printArrayList() {
-            ArrayList<Tile> tileList = getArrayList();
-            for (Tile tile : tileList) {
-                System.out.println(tile.toString());
+        public void printBoard() {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    System.out.print(matrix[i][j].getTileType() + " ");
+                }
+                System.out.println();
             }
         }
     }
+
+
 
