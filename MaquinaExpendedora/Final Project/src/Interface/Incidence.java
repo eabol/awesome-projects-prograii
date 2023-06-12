0fb619b29d1e006feb864914b87773e0b20d9166
paulaquing <paula.diestro@alumnos.uneatlantico.es>
2023-06-12 18:14:54 +0200

    docs(final proyect): final commit

--- MaquinaExpendedora/Final Project/src/Interface/Incidence.java
+++
@@ -1,5 +0,0 @@
-package Interface;
-
-public interface Incidence {
-    public void setDate();
-}
--- MaquinaExpendedora/Final Project/src/Interface/State.java
+++
@@ -1,7 +0,0 @@
-package Interface;
-
-public enum State {
-    JAM,FAULT,NO_STOCK,NO_CHANGE,NORMAL;
-
-
-}
