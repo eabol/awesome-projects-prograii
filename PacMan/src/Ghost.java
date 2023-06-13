24b2d9057e2b8a6cbb7cd37305b05eb71cd7a3e1
DiegoMusi <104350963+DiegoMusi@users.noreply.github.com>
2023-06-13 16:39:11 +0200

    Cambios Clases
    Seg commit

--- PacMan/src/Ghost.java
+++ PacMan/src/Ghost.java
@@ -1,14 +1,40 @@
-public abstract class Ghost extends Enemy implements Droweable, Movable{
+public abstract class Ghost extends Enemy implements Droweable, Movable {
     public Ghost(int x, int y) {
         super(x, y);
     }
 
     @Override
-    public void Draw() {
+    public void movement(PacMan pacman,Map map) {
+        int pacmanX = pacman.getX();
+        int pacmanY = pacman.getY();
 
+        // calcular las distancias para cada movimiento posible
+        int distMoveRight = Math.abs((this.x+1) - pacmanX) + Math.abs(this.y - pacmanY);
+        int distMoveLeft = Math.abs((this.x-1) - pacmanX) + Math.abs(this.y - pacmanY);
+        int distMoveUp = Math.abs(this.x - pacmanX) + Math.abs((this.y-1) - pacmanY);
+        int distMoveDown = Math.abs(this.x - pacmanX) + Math.abs((this.y+1) - pacmanY);
+
+        // encontrar el movimiento que minimiza la distancia
+        int minDist = Math.min(Math.min(distMoveRight, distMoveLeft), Math.min(distMoveUp, distMoveDown));
+
+        if (minDist == distMoveRight) {
+            if (!map.isWall(this.x + 1, this.y))
+                this.x++;
+        } else if (minDist == distMoveLeft) {
+            if (!map.isWall(this.x - 1, this.y))
+                this.x--;
+        } else if (minDist == distMoveUp) {
+            if (!map.isWall(this.x, this.y - 1))
+                this.y--;
+        } else if (minDist == distMoveDown) {
+            if (!map.isWall(this.x, this.y + 1))
+                this.y++;
+        }
     }
+
     @Override
-    public void movement() {
-
+    public void Draw() {
+        System.out.print("G");
     }
+
 }
--- PacMan/src/PowerUp.java
+++ PacMan/src/PowerUp.java
@@ -1,15 +1,15 @@
 public class PowerUp extends Point implements Droweable{
 
-    private int duration;
+    private int duration=15;
 
-    public PowerUp(int x, int y,int value,int duration){
-        super(x,y,value);
-        this.duration = duration;
+    public PowerUp(int x, int y){
+        super(x,y,6);
+
     }
 
     @Override
     public void Draw() {
-
+        System.out.print("+");
     }
 
 
