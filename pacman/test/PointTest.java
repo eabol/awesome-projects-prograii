ce9bae95a6c69e20d678dcf92d4844c0f1630436
Sergio Mayen <sergio.mayen@ct.uneatlantico.es>
2023-06-13 23:26:45 +0200

    fix: Final changes to the uml diagram

--- pacman/test/PointTest.java
+++ pacman/test/PointTest.java
@@ -1,22 +1,20 @@
-package pacman.test;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class PointTest {
-
     @Test
     public void testPacmanComerComida() {
         // Preparación del Pacman
         Pacman pacman = new Pacman();
         int puntajeInicial = pacman.getPuntaje();
         Comida comida = new Comida();
-        
+
         // Ejecución del método para obtener el puntaje
         pacman.comerComida(comida);
         int puntajeFinal = pacman.getPuntaje();
-        
+
         // Verificación del resultado
         Assert.assertEquals(puntajeInicial + comida.getPuntos(), puntajeFinal);
     }
-}
 
+}
